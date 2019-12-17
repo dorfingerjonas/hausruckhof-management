57df5a16123c016fc17a78e271876822f285081e
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2019-12-17 21:29:48 +0100

    start to create classes

---
+++ script/horse.js
@@ -0,0 +1,7 @@
+class Horse {
+    constructor (name, id, timetable) {
+        this.name = name;
+        this.id = id;
+        this.timetable = timetable;
+    }
+}
---
+++ script/room.js
@@ -0,0 +1,8 @@
+class Room {
+    constructor (number, capacity, unusedBeds, children) {
+        this.number = number;
+        this.capacity = capacity;
+        this.unusedBeds = unusedBeds;
+        this.children = children;
+    }
+}
