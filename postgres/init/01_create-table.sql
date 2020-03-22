7810b6341a46f438332113def9d19478277c0ceb
user <jonas.dorfinger@gmx.at>
2020-03-22 15:07:53 +0100

    create child table

--- postgres/init/01_create-table.sql
+++ postgres/init/01_create-table.sql
@@ -21,3 +21,22 @@ CREATE TABLE employee (
     id VARCHAR(10),
     CONSTRAINT pk_employee PRIMARY KEY (id)
 );
+
+CREATE TABLE child (
+	first_name VARCHAR(80) NOT NULL,
+	last_name VARCHAR(80) NOT NULL,
+    birthday DATE NOT NULL,
+	svnr VARCHAR(15) NOT NULL,
+    street VARCHAR(80),
+    post_code INT,
+    housenumber VARCHAR(10),
+    place_name VARCHAR(80),
+	gender VARCHAR(20),
+	email VARCHAR(80),
+	phonenumber1 VARCHAR(80),
+	phonenumber1_owner VARCHAR(80),
+	phonenumber2 VARCHAR(80),
+	phonenumber2_owner VARCHAR(80),
+	allergens VARCHAR(80),
+	CONSTRAINT pk_child PRIMARY KEY (svnr)
+);
