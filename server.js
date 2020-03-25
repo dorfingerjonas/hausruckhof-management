e15befa838812055d6decb88c047af24f017edd6
user <jonas.dorfinger@gmx.at>
2020-03-25 22:51:17 +0100

    add updateChild post-handler

--- server.js
+++ server.js
@@ -46,13 +46,34 @@ app.get('/children', (req, respond) => {
   respond.header("Content-Type",'application/json');
   
   client
-    .query('SELECT * FROM child')
+    .query('SELECT * FROM child ORDER BY last_name')
     .then((res) => {
       if (res.rowCount > 0) {        
         respond.send(res.rows);
       } else {
         respond.send(false);
       }
+    }).catch(e => console.error(e.message));
+});
+
+app.post('/updateChild', (req, respond) => {  
+  respond.set('Access-Control-Allow-Origin', '*');
+  respond.set('Access-Control-Allow-Headers', 'Content-Type')
+  respond.header("Content-Type",'application/json');
+  const data = req.body;
+
+  console.log(data.birthday);
+
+  const query = {
+    name: 'update-child-in-db',
+    text: "UPDATE CHILD SET first_name=$1, last_name=$2, birthday=TO_DATE($3, 'YYYY-MM-DD'), svnr=$4, street=$5, housenumber=$6, post_code=$7, place_name=$8, gender=$9, email=$10, phonenumber1=$11, phonenumber1_owner=$12, phonenumber2=$13, phonenumber2_owner=$14, allergens=$15 WHERE svnr=$4",
+    values: [data.first_name, data.last_name, data.birthday, data.svnr, data.street, data.housenumber, data.post_code, data.place_name, data.gender, data.email, data.phonenumber1, data.phonenumber1_owner, data.phonenumber2, data.phonenumber2_owner, data.allergens],
+  }
+  
+  client
+    .query(query)
+    .then((res) => {
+      res.rowCount === 1 ? respond.send(true) : respond.send(false);
     }).catch(e => console.error(e.message));
 });
 
